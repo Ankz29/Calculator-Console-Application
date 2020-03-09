d9a01086133e2fb3fe4244dbd0426213aebfec93
Ankz29 <muddapurankita29@gmail.com>
2020-03-09 20:43:12 +0530

    Adding files for arithematic operations.

---
+++ CalculatorUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    public class CalculatorUtility
+    {
+       static int result = 0;
+        public static int CalculatorMethod(int value1, int value2, int Operator)
+        {
+
+           switch(Operator)
+            {
+                case 1:                    
+                      result = Addition(value1, value2);                  
+                    break;
+
+                case 2:
+                    result = Subtraction(value1, value2);
+                    break;
+
+                case 3:
+                    result = Multiplication(value1, value2);
+                    break;
+
+                case 4:
+                    result = Division(value1, value2);
+                    break;
+
+                default :
+                    Console.Write("Oops, you have entered wrong Arithematic symbol");
+                    break;  
+            }
+
+            return result;
+        }
+
+
+        public static int Addition(int val1, int val2)
+        {
+            result = val1 + val2;
+            return result;
+        }
+
+        public static int Subtraction(int val1, int val2)
+        {
+            result = val1 - val2;
+            return result;
+        }
+
+        public static int Multiplication(int val1, int val2)
+        {
+            result = val1 * val2;
+            return result;
+        }
+
+        public static int Division(int val1, int val2)
+        {
+            result = val1 % val2;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorConsole;
+namespace CalculatorConsole
+{
+    class Program
+    {
+        public static int variable_A;
+        public static int variable_B;
+        public static int Operator;
+        static void Main(string[] args)
+        {
+            Console.Write("enter 1 for addition: "+"\n"); 
+            Console.Write("enter 2 for subtraction: "+"\n");
+            Console.Write("enter 3 for multiplication: "+"\n");
+            Console.Write("enter 4 for divison: "+"\n");
+
+            Operator = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter 1st value: ");
+            variable_A = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter 2nd value: ");
+            variable_B = Convert.ToInt32(Console.ReadLine());
+
+           var data =  CalculatorUtility.CalculatorMethod(variable_A, variable_B, Operator);
+
+            Console.Write("The resultant is {0}", data);
+
+            Console.ReadLine();
+        }
+    }
+}
